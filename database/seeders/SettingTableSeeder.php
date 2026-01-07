720ede834328d4bcd0fdf6e6a2daf5ca25a959cd
Abdul Rehman <abdul.rehman@alluredigital.net>
2026-01-07 14:49:19 +0500

    c3

--- database/seeders/SettingTableSeeder.php
+++ database/seeders/SettingTableSeeder.php
@@ -18,9 +18,9 @@ class SettingTableSeeder extends Seeder
         DB::table('setting')->insert([
             'setting_id'       => 1,
             'branch_id'        => 1,
-            'company_name'     => 'Toko Ku',
-            'address'          => 'Jl. Kibandang Samaran Ds. Slangit',
-            'phone'            => '081234779987',
+            'company_name'     => 'AlHaseeb Traders',
+            'address'          => 'Bharia Town Phase 4, Rawalpindi, Pakistan',
+            'phone'            => '03001234567',
             'receipt_type'     => 1,
             'discount'         => 5,
             'logo_path'        => '/img/logo.png',
